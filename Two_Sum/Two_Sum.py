30a6cd298b460c33c5a6ec643a2e9e06b01fde48
jerrypy <jerrynwin@gmail.com>
2015-11-23 22:11:17 +0800

    add o(n) hash table solution

--- Two_Sum/Two_Sum.py
+++ Two_Sum/Two_Sum.py
@@ -1,7 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
         """
-        O(n^2) Brute force
+		Brute Force
+
+        Time Complexity | Space Complexity |  Run Time
+           O(n^2)           O(1)                4944ms 
 
         :type nums: List[int]
         :type target: int
@@ -11,3 +14,18 @@ class Solution(object):
             for index_j, j in enumerate(nums[index_i+1:]):
                 if j + i == target:
                     return [index_i + 1, index_j + index_i + 2] 
+
+	def twoSum(self, nums, target):
+		"""
+		Hash Table
+
+        Time Complexity | Space Complexity |  Run Time
+           O(n)           O(n)                 40ms
+
+		Create a hash table, the difference between target and the first element(the num		-ber we need) stores as key, and index of first element as value.
+		"""
+		ht = dict()
+		for index_i, i in enumerate(nums):
+			if i in ht: # if this number is needed
+				return [ht[i] + 1, index_i + 1]	
+			ht[target - i] = index_i
